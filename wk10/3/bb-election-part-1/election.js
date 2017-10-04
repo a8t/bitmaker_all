41cd0d4d118d71027b464fcbc5f8701e8296b2d3
Andy Tran <andy8tran@gmail.com>
2017-10-04 13:46:53 -0400

    added li element template

--- wk10/3/bb-election-part-1/election.js
+++ wk10/3/bb-election-part-1/election.js
@@ -1,5 +1,15 @@
 document.addEventListener("DOMContentLoaded", function() {
 
-  // Imagination!
+  const list = document.querySelector('#list')
+
+  fetch("https://bb-election-api.herokuapp.com/")
+    .then(response => response.json())
+    .then(respJSON => {
+      respJSON.candidates.forEach(eachCandidate => {
+        list.innerHTML += `
+          <li> Name: ${eachCandidate.name}; Votes: ${eachCandidate.votes} </li>
+        `
+      })
+    })
 
 });
