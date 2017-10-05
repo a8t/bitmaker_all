570cc25e0eb65b1180bf40031aea9310e09cc38f
Andy Tran <andy8tran@gmail.com>
2017-10-05 16:50:03 -0400

    changed votes to asynchronous

--- wk10/3/bb-election-part-1/election.js
+++ wk10/3/bb-election-part-1/election.js
@@ -1,15 +1,55 @@
 document.addEventListener("DOMContentLoaded", function() {
 
   const list = document.querySelector('#list')
+  const form = document.querySelector('#form')
 
-  fetch("https://bb-election-api.herokuapp.com/")
+  const getVotes = () => {fetch("https://bb-election-api.herokuapp.com/")
+    .then(response => response.json())
+    .then(respJSON => {
+      list.innerHTML = ""
+      respJSON.candidates.forEach(eachCandidate => {
+        list.innerHTML += `<li> Name: ${eachCandidate.name}; Votes: ${eachCandidate.votes} </li>`   
+      })
+    })
+  }
+
+  const getForm = () => {
+    fetch("https://bb-election-api.herokuapp.com/")
     .then(response => response.json())
     .then(respJSON => {
       respJSON.candidates.forEach(eachCandidate => {
-        list.innerHTML += `
-          <li> Name: ${eachCandidate.name}; Votes: ${eachCandidate.votes} </li>
+        form.innerHTML += `
+        <input type="radio" class="votes" name="candidate" value="${eachCandidate.name}" id="${eachCandidate.name}">
+        <label for=${eachCandidate.name}>${eachCandidate.name}</label></br >
         `
       })
+      form.innerHTML += `<button id="submit" type="submit">Vote!</button>`
     })
+  }
+
+  const voteFor = (candidate) => {
+    fetch(`https://bb-election-api.herokuapp.com/vote?name=${candidate}`, {
+      method: "POST"
+    })
+  }
+  
+  getForm()
+  getVotes()
+
+
+  form.onsubmit = (e) => {
+    e.preventDefault()
+    let vote
+    document.querySelectorAll('.votes').forEach((each) => {
+      if (each.checked) vote = each.value
+    })
+
+    if (vote) {
+      document.querySelector('#submit').disabled = "true"
+
+      voteFor(vote)
+      getVotes()
+    }
+  }
 
 });
